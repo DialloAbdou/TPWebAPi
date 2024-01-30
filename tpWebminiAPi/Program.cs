29401404724731bd1583c97a26c5ac1ed77c9bd5
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-01-30 20:27:52 +0100

    liste de todo

--- tpWebminiAPi/Program.cs
+++ tpWebminiAPi/Program.cs
@@ -5,5 +5,9 @@ var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 app.MapGet("/hello", () => "hello Abdou");
 app.MapGet("/todo/1", () => new Todo { Id = 1, Titre = "formation Api", StartDate = DateTime.Today });
-
+app.MapGet("/todo/", () =>
+{
+     var todo = new TodoService().getAllToDo();
+      return Results.Ok(todo);
+});
 app.Run();
