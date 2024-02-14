56858b94e34f55f4ceee1c6a9d6769ff601bdf1e
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-02-14 22:27:11 +0100

    tp realisation todo

--- tpWebminiAPi/Program.cs
+++ tpWebminiAPi/Program.cs
@@ -1,13 +1,36 @@
 ﻿using tpWebminiAPi.Todo;
 
 var builder = WebApplication.CreateBuilder();
-
+builder.Services.AddSingleton<TodoService>();
 var app = builder.Build();
 app.MapGet("/hello", () => "hello Abdou");
-app.MapGet("/todo/1", () => new Todo { Id = 1, Titre = "formation Api", StartDate = DateTime.Today });
-app.MapGet("/todo/", () =>
+app.MapGet("/todos/1", () => new Todo { Id = 1, Titre = "formation Api", StartDate = DateTime.Today });
+app.MapGet("/todos/", ( TodoService todoService) =>
 {
-     var todo = new TodoService().getAllToDo();
-      return Results.Ok(todo);
+     var todos= todoService.getAllToDo();
+      return Results.Ok(todos);
+});
+app.MapGet("/todos/{id:int}",(int id, TodoService todoService)=>{
+    var todo = todoService.getAllToDo().Find(t => t.Id == id);
+    if(todo is not null)
+    {
+        return Results.Ok(todo);
+    }else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapGet("/todos/active", (TodoService todoService) =>
+{
+    var todoActive = todoService.getAllToDo().Where(t=>t.EndDate is null);
+
+    if(todoActive is not null)
+    {
+        return Results.Ok(todoActive);
+    }else
+    {
+        return Results.NotFound();
+    }
 });
 app.Run();
--- tpWebminiAPi/Todo/TodoService.cs
+++ tpWebminiAPi/Todo/TodoService.cs
@@ -3,17 +3,38 @@
     public class TodoService
     {
 
-        IEnumerable<Todo> TodoList = new List<Todo>()
+        List<Todo> TodoList = new List<Todo>()
         {
             new Todo{ Id = 1, Titre = "Formation WebApi", StartDate = DateTime.Today },
-            new Todo{ Id = 1, Titre = "Formation WebApi", StartDate = DateTime.Today.AddDays(1) },
-            new Todo{ Id = 1, Titre = "Formation WebApi", StartDate = DateTime.Today .AddDays(2)},
+            new Todo{ Id = 2, Titre = "Formation Fondamenteau", StartDate = DateTime.Today.AddDays(1) },
+            new Todo{ Id = 3, Titre = "Formation WebApi", StartDate = DateTime.Today .AddDays(2)},
+            new Todo{ Id = 4, Titre = "Formation WebApi", StartDate = DateTime.Today, EndDate = DateTime.Today.AddDays(2)},
+
 
         };
 
-        public IEnumerable<Todo> getAllToDo()
+        public List<Todo> getAllToDo()
         {
             return TodoList;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="titre"></param>
+        /// <returns></returns>
+        public Todo AddToDo(string titre)
+        {
+            var idMax = TodoList.Max(t => t.Id) + 1;
+            Todo todo = new Todo
+            {
+                Id = idMax,
+                Titre = titre,
+                StartDate = DateTime.Today,
+            };
+            return todo;
+
+        }
+
     }
 }
